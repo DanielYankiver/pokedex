92a19a3855f21ff4d3c50b8b200166fa451c2955
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-11-20 19:55:42 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import pokeball from './pokeball.png';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">
+        <img src={pokeball} className="pokeball" alt="logo" />
+      </div>
+      <div className="pokedex">
+        {/* <div className="search-bar">
+          Search Bar
+        </div> */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
+
