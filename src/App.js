e85bb09c5bc5903323e36fafd6b989d9d7a92645
Daniel Yankiver <69213153+DanielYankiver@users.noreply.github.com>
2022-11-21 17:51:51 -0500

    built search, that pulls up name and image

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
+import React, { useState, useEffect } from 'react';
 import logo from './pokemon-logo.png';
 import pokeball from './pokeball.png';
+import pokemonService from './pokemonService';
 import './App.css';
 
 function App() {
+  const [pokemon, setPokemon] = useState('');
+  const [details, setDetails] = useState(null);
+
+  const handleClick = async () => {
+      try {
+      const responce = await pokemonService.detail(pokemon)
+      setDetails(responce);
+    } catch (err) {
+      setDetails({ error: "pokemon not found" });
+    }
+  };
+
   return (
     <div className="App">
-      <div className="header">
-        <div className="header-wrapper">
-          <img src={logo} className="pokemon-logo" alt="logo" />
+      <div className="body">
+        <div className="body-wrapper">
+          <div className="pokeball-logo-wrapper">
+            <img src={logo} className="pokemon-logo" alt="logo" />
+          </div>
           <div className="pokeball-wrapper">
             <img src={pokeball} className="pokeball" alt="logo" />
           </div>
         </div>
-      </div>
-      {/* <div className="header">
-      </div> */}
-      {/* <div className="pokedex">
-        <div className="search-bar">
-          Search Bar
+        <div className="pokedex-container">
+        <div className='pokedex'>
+          <input value={pokemon} onChange={evt => setPokemon(evt.target.value)} />
+          <button onClick={handleClick}>Search</button>
+          { details && (
+            details.error ? (
+              <h1>{details.error}</h1>
+            ) : (
+              <div>
+                <h1>{details.name}</h1>
+                <img src={details.sprites.front_default} alt="lolsorry" />
+              </div>
+          ))}
         </div>
-      </div> */}
+
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/pokemonService.js
@@ -0,0 +1,9 @@
+const BASE_URL = "https://pokeapi.co/api/v2/";
+
+export default {
+  detail
+}
+
+function detail(pokemon) {
+  return fetch(`${BASE_URL}pokemon/${pokemon}`).then((res) => res.json());
+}
